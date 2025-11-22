6de1768e198cafa742e7e7d61d8de2eb53889320
JaVi080 <157447257+JaVi080@users.noreply.github.com>
2025-11-23 00:17:54 +0500

    Time: 0 ms (100.00%) | Memory: 46.8 MB (14.02%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,18 +11,17 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-            ListNode current=head;
-        int index=0;
-        while(current!=null){
-            index++;
-            ListNode prv=head;
-            for(int i=0;i<index;i++){
-                if(current.next==prv){
-                    return true;
-                }
-                prv=prv.next;
-            }
-            current=current.next;
+                   if(head==null||head.next==null)return false;
+        ListNode slow_pointer=head;
+        ListNode fast_pointer=head;
+     
+        while(fast_pointer != null && fast_pointer.next != null){
+
+                slow_pointer=slow_pointer.next;
+                fast_pointer=fast_pointer.next.next;
+            
+              if(slow_pointer==fast_pointer)return true;
+          
         }
         return false;
     }
